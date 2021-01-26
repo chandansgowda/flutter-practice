35c9560b9acb349cc9b34d04aab7726c3b21332b
Chandan Gowda <chandansuresh007@gmail.com>
2021-01-26 13:15:42 +0530

    Worked with Rows & Columns

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -21,31 +21,56 @@ class HomePage extends StatelessWidget {
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.greenAccent, Colors.lightBlue]
+              colors: [Colors.greenAccent , Colors.lightBlue]
             )
           ),
         ),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Container(
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [BoxShadow(color: Colors.grey[400], blurRadius: 8)],
-            //borderRadius: BorderRadius.circular(8),
-            gradient: LinearGradient(
-              colors: [Colors.lightBlue, Colors.greenAccent],
+          color: Colors.black87,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                  //color: Colors.red,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.lightGreen
+                  ),
+                ),
+              ],
             )
           ),
-          child: Text("ABCD", style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-          ),),
         ),
-
       ),
     );
   }
 }
+
